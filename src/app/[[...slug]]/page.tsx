a5f9001a6efd2d6965b7e6a2284a10bc8cba2113
cstef <colin@petit-suisse.fr>
2024-07-20 13:52:09 +0200

    feat: add default page

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,5 +1,9 @@
 import Post from "@/components/post";
-import { DEFAULT_BRANCH, DEFAULT_REPO } from "@/lib/constants";
+import {
+	DEFAULT_BRANCH,
+	DEFAULT_REPO,
+	DEFAULT_REPO_DATA,
+} from "@/lib/constants";
 import nodepath from "node:path";
 import { compileMDX } from "@/lib/mdx";
 import { fetchPost } from "@/lib/post";
@@ -10,6 +14,7 @@ import { headers } from "next/headers";
 import ThemeSelect from "@/components/theme-select";
 import { Rss } from "lucide-react";
 import { readingTime } from "reading-time-estimator";
+import { isErrorHasStatus } from "@/lib/github";
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -67,11 +72,61 @@ function getRepo(): {
 	return null;
 }
 
-const Page: NextPage = async () => {
-	const repoData = getRepo();
+const fetchData = async (
+	request: GitHubRequest,
+): Promise<
+	| {
+			content: string;
+			lastCommit: {
+				date: string;
+				author: {
+					name: string;
+					login: string;
+					avatar: string;
+				};
+			} | null;
+			error: undefined;
+	  }
+	| {
+			content: null;
+			lastCommit: null;
+			error: string;
+	  }
+> => {
+	try {
+		const { content, lastCommit } = await (["mdx", "md"].includes(
+			request.path.split(".").pop() ?? "",
+		)
+			? fetchPost
+			: fetchReadme)(request);
 
-	if (!repoData) {
-		return <div>Invalid repo</div>;
+		return { content, lastCommit, error: undefined };
+	} catch (
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		error: any
+	) {
+		console.error("Error fetching data", error);
+		if (isErrorHasStatus(error)) {
+			switch (error.status) {
+				case 404:
+					return { content: null, lastCommit: null, error: "Not found" };
+				default:
+					return {
+						content: null,
+						lastCommit: null,
+						error: `Error status ${error.status}`,
+					};
+			}
+		}
+		return { content: null, lastCommit: null, error: error.toString() };
+	}
+};
+
+const Page: NextPage = async () => {
+	let repoData = getRepo();
+
+	if (!repoData || !repoData.owner) {
+		repoData = DEFAULT_REPO_DATA;
 	}
 
 	const request: GitHubRequest = {
@@ -80,13 +135,30 @@ const Page: NextPage = async () => {
 		path: repoData.path ?? "",
 	};
 
-	const { content, lastCommit } = await (["mdx", "md"].includes(
-		request.path.split(".").pop() ?? "",
-	)
-		? fetchPost
-		: fetchReadme)(request);
-
+	const { content, lastCommit, error } = await fetchData(request);
+	console.log("content", content);
 	console.log("lastCommit", lastCommit);
+
+	if (error !== undefined) {
+		return (
+			<div className="flex w-screen h-screen justify-center items-center">
+				<div className="text-center flex gap-2 flex-col">
+					<h1 className="text-4xl">
+						An error occured: <b>{error}</b>
+					</h1>
+					<p className="text-2xl dark:text-gray-400 text-gray-600">
+						<a
+							href={`https://github.com/${repoData.owner}/${repoData.repo}/tree/${repoData.branch}/${repoData.path}`}
+							target="_blank"
+							rel="noreferrer"
+						>
+							Check the repository and path provided in the URL.
+						</a>
+					</p>
+				</div>
+			</div>
+		);
+	}
 
 	const mdx = await compileMDX(content, {
 		asset: (url) => {
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,9 @@
 export const DEFAULT_REPO = "blog";
 export const DEFAULT_BRANCH = "main";
 export const README_FILES = ["README.md", "index.md"];
+export const DEFAULT_REPO_DATA = {
+	owner: "cestef",
+	repo: "uses.ink",
+	branch: DEFAULT_BRANCH,
+	path: "blog",
+};
