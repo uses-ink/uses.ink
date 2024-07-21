a4077da0ac5b9897778f4e34bd5395d75321f34e
cstef <colin@petit-suisse.fr>
2024-07-21 19:52:37 +0200

    feat: add word wrap toggle to pre

--- src/components/pre.tsx
+++ src/components/pre.tsx
@@ -1,8 +1,8 @@
 // Based on https://github.com/facebook/docusaurus/blob/ed9d2a26f5a7b8096804ae1b3a4fffc504f8f90d/packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx
 // which is under MIT License as per the banner
 
-import { useRef, useState } from "react";
-import { Clipboard, Check } from "lucide-react";
+import { useCallback, useRef, useState } from "react";
+import { Clipboard, Check, WrapText } from "lucide-react";
 const CodeBlock = ({
 	children,
 	...props
@@ -27,23 +27,44 @@ const CodeBlock = ({
 		setTimeout(() => setShowCopied(false), 2000);
 	};
 
+	const toggleWordWrap = useCallback(() => {
+		const htmlDataset = document.documentElement.dataset;
+		const hasWordWrap = "wordWrap" in htmlDataset;
+		if (hasWordWrap) {
+			// biome-ignore lint/performance/noDelete: need to delete
+			delete htmlDataset.wordWrap;
+		} else {
+			htmlDataset.wordWrap = "";
+		}
+	}, []);
+
 	return (
 		<div className="relative group">
 			<pre {...props} ref={pre}>
 				{children}
 			</pre>
-			<button
-				type="button"
-				aria-label={"Copy code to clipboard"}
-				className="group-hover:opacity-100 appearance-none border dark:border-gray-700 border-gray-200 cursor-pointer dark:bg-[--shiki-background] rounded-md dark:text-gray-400 opacity-0 select-none py-2 px-2 absolute right-2 top-2 transition-opacity duration-200 ease-in-out bg-white text-gray-800"
-				onClick={handleCopyCode}
-			>
-				{showCopied ? (
-					<Check className="w-4 h-4" />
-				) : (
-					<Clipboard className="w-4 h-4" />
-				)}
-			</button>
+			<div className="absolute right-2 top-2">
+				<button
+					type="button"
+					aria-label={"Copy code to clipboard"}
+					className="md:hidden group-hover:opacity-100 appearance-none border dark:border-gray-700 border-gray-200 cursor-pointer dark:bg-[--shiki-background] rounded-md dark:text-gray-400 opacity-0 select-none py-2 px-2 transition-opacity duration-200 ease-in-out bg-white text-gray-800"
+					onClick={toggleWordWrap}
+				>
+					<WrapText className="w-4 h-4" />
+				</button>
+				<button
+					type="button"
+					aria-label={"Copy code to clipboard"}
+					className="group-hover:opacity-100 appearance-none border dark:border-gray-700 border-gray-200 cursor-pointer dark:bg-[--shiki-background] rounded-md dark:text-gray-400 opacity-0 select-none py-2 px-2 transition-opacity duration-200 ease-in-out bg-white text-gray-800"
+					onClick={handleCopyCode}
+				>
+					{showCopied ? (
+						<Check className="w-4 h-4" />
+					) : (
+						<Clipboard className="w-4 h-4" />
+					)}
+				</button>
+			</div>
 		</div>
 	);
 };
