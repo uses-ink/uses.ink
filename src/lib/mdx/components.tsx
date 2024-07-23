18690cdd48e0555085b553b825c965de0ab45cf2
cstef <colin@petit-suisse.fr>
2024-07-23 12:05:37 +0200

    feat: add heading anchors with rehype-slug

--- src/lib/mdx/components.tsx
+++ src/lib/mdx/components.tsx
@@ -13,4 +13,50 @@ export const mdxComponents = {
 	// https://github.com/mdx-js/mdx/discussions/1921
 	head: Head as any,
 	pre: Pre as any,
+	h1: ({ children, ...props }: any) => (
+		<h1 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h1>
+	),
+	h2: ({ children, ...props }: any) => (
+		<h2 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h2>
+	),
+	h3: ({ children, ...props }: any) => (
+		<h3 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h3>
+	),
+	h4: ({ children, ...props }: any) => (
+		<h4 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h4>
+	),
+	h5: ({ children, ...props }: any) => (
+		<h5 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h5>
+	),
+	h6: ({ children, ...props }: any) => (
+		<h6 {...props} className="group">
+			{children}
+			<Anchor id={props.id} />
+		</h6>
+	),
 };
+
+const Anchor = ({ id }: any) => (
+	<a
+		href={`#${id}`}
+		className="text-muted-foreground opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+		style={{ marginLeft: "0.25rem", textDecoration: "none" }}
+	>
+		#
+	</a>
+);
