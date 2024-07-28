1e26b696281b16130d74a9e0a7b6163ba8bb7964
cstef <colin@petit-suisse.fr>
2024-07-28 02:14:08 +0200

    fix: don't use single-tilde gfm strikethrough

--- src/lib/mdx/index.ts
+++ src/lib/mdx/index.ts
@@ -43,7 +43,7 @@ export async function compileMDX(
 		format: "md",
 		outputFormat: "function-body",
 		remarkPlugins: [
-			remarkGfm,
+			[remarkGfm, { singleTilde: false }],
 			[remarkFrontmatter, { marker: "-", type: "yaml" }],
 			[remarkMdxFrontmatter, { name: "matter" }],
 			remarkMath,
