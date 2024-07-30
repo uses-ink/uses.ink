21c969327cd75c7d5caeb967e72017caa68acba4
cstef <colin@petit-suisse.fr>
2024-07-30 13:25:26 +0200

    fix: try to find a workaround for typst

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
 import nextBuildId from "next-build-id";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	experimental: {
+		serverComponentsExternalPackages: ["@myriaddreamin/typst-ts-node-compiler"],
+	},
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ["@svgr/webpack"],
 		});
-
 		// Important: return the modified config
 		return config;
 	},
