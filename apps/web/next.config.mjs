c71a513f7dd5d4c6eb7003ec04d79509c92504f6
cstef <colin@petit-suisse.fr>
2024-08-21 01:03:35 +0200

    refactor: begin transfering docker

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -5,31 +5,16 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-import { readdirSync } from "node:fs";
-import { join } from "node:path";
-import { builtinModules } from "node:module";
-
-const monorepoModules = (scope = undefined) => {
-	const nodeModulesPath = join(__dirname, "node_modules");
-	return readdirSync(scope ? join(nodeModulesPath, scope) : nodeModulesPath, {
-		withFileTypes: true,
-	})
-		.filter((dirent) => dirent.isDirectory() && !dirent.name.startsWith("."))
-		.map((dirent) => (scope ? `${scope}/${dirent.name}` : dirent.name))
-		.reduce(
-			(acc, name) =>
-				undefined === scope && name.startsWith("@")
-					? // biome-ignore lint/performance/noAccumulatingSpread: <explanation>
-						[...acc, ...monorepoModules(name)]
-					: // biome-ignore lint/performance/noAccumulatingSpread: <explanation>
-						[...acc, name],
-			[],
-		);
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "standalone",
+	experimental: {
+		serverComponentsExternalPackages: [
+			"@myriaddreamin/typst-ts-node-compiler",
+			"@node-rs/xxhash",
+			"pikchr-wasm",
+		],
+	},
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.module.rules.push({
 			test: /\.svg$/,
