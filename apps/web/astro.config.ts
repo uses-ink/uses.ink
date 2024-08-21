3418dce7ee75761563127bc78c404681a5e78df1
cstef <colin@petit-suisse.fr>
2024-08-22 01:59:45 +0200

    fix: add docker stuff

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,13 @@ export default defineConfig({
 		}),
 		react(),
 	],
-
 	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	vite: {
+		ssr: {
+			external: ["@node-rs/xxhash", "@myriaddreamin/typst-ts-node-compiler"],
+		},
+	},
 });
