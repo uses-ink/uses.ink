c4699e1b02d809f977b4c380f3e11ccd5104c7c3
cstef <root@cstef.dev>
2024-09-18 09:19:34 +0200

    fix: postinstall script to also work on x64 systems and remove the correct file based on the current system

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,20 +1,31 @@
+#!/bin/bash
+
 # Cleanup @myriaddreamin/typst-ts-node-compiler
 # it downloads both typst-ts-node-compiler.linux-arm64-musl.node and typst-ts-node-compiler-linux-arm64-gnu.node
+# it can also download x64 versions
 # But we only need one of them, so we remove the other one to save space
 
 # Check if we have both files
 BASEPATH=$(pwd)
 cd node_modules/@myriaddreamin
-MUSL_PATH=$(find . -name "typst-ts-node-compiler-linux-arm64-musl")
-GNU_PATH=$(find . -name "typst-ts-node-compiler-linux-arm64-gnu")
+MUSL_PATH=$(find . -regex '.*typst-ts-node-compiler.*musl.*')
+GNU_PATH=$(find . -regex '.*typst-ts-node-compiler.*gnu.*')
 
 if [ -z "$MUSL_PATH" ] || [ -z "$GNU_PATH" ]; then
     echo "No musl and gnu files found, skipping ($MUSL_PATH, $GNU_PATH)"
     exit 0
 fi
 
-echo "Both musl and gnu files found, removing gnu file"
+# Determine which one to remove
+# Are we running on musl?
+if ldd --version | grep -q musl; then
+    echo "Running on musl"
+    REMOVE_PATH=$GNU_PATH
+else
+    echo "Running on gnu"
+    REMOVE_PATH=$MUSL_PATH
+fi
 
-rm -rf $GNU_PATH
+rm -rf $REMOVE_PATH
 
-echo "Done"
+echo "Removed $REMOVE_PATH"
